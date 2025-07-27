02b24aec69a4b66e4b93a9c1478a4ed7d5556ee9
Lucas Rozalem <lrozalem@gmail.com>
2025-07-27 17:23:23 -0300

    feat: adiciona ceo de forma dinamica

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,33 +1,50 @@
-import { BlogList } from '@/templates/blog';
+import { PostPage } from '@/templates/blog';
 import { allPosts } from 'contentlayer/generated';
-import type { Metadata } from 'next';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
-export const metadata: Metadata = {
-  title: 'Blog',
-  description: 'Dicas e estratégias para impulsionar seu negócio',
-  robots: 'index, follow',
-  openGraph: {
-    title: 'Blog',
-    description: 'Dicas e estratégias para impulsionar seu negócio',
-    url: 'https://site-blog-nextjs.vercel.app/og-image.jpg',
-    siteName: 'Site.Set',
-    locale: 'pt-BR',
-    type: 'website',
-    images: [
-      {
-        url: 'https://site-blog-nextjs.vercel.app/og-image.jpg',
-        width: 800,
-        height: 600,
-        alt: 'Site.Set',
-      },
-    ],
-  },
+type BlogPostPageProps = {
+  params: Promise<{
+    slug: string;
+  }>;
 };
 
-export default function BlogListPage() {
-  const sortedPosts = allPosts.sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-  );
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
 
-  return <BlogList posts={sortedPosts} />;
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: 'index, follow',
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
+
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return <PostPage post={post} />;
 }
