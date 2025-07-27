3c4a72bbe02d51f6efc7f645a614a00a817b275d
Lucas Rozalem <lrozalem@gmail.com>
2025-07-27 17:12:10 -0300

    feat: add seo in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
 import { LandingPage } from "@/templates/landing-page";
+import { Metadata } from "next";
 // import { ErrorComponent } from "./component";
-
+export const metadata: Metadata = {
+  title: "Site.Set",
+  description: "Venda seus produtos como afiliado em um único lugar",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.Set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "site-blog-nextjs.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt-BR",
+    type: "website",
+    images: [
+      {
+        url: "site-blog-nextjs.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 export default function HomePage() {
   return (
     <>
